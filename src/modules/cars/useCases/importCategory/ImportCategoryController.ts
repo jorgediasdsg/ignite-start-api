3df7f944409fb8065cce707501b1aeee2efa0969
Jorge Dias <jorgel@cbm.sc.gov.br>
2021-04-17 20:16:32 -0300

    ADD: Read CSV upload

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from 'express';
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
+
+
+
+class ImportCategoryController {
+    constructor(private importVategoryUseCase:ImportCategoryUseCase){}
+    handle(request: Request, response: Response): Response {
+        const {file} = request;
+            this.importVategoryUseCase.execute(file)
+        return response.send();
+    }
+}
+
+export {ImportCategoryController};
