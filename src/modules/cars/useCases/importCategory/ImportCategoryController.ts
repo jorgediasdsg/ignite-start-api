4043a83b1d168444c60b9c7dd5dc20d9e7e7d8a3
Jorge Dias <jorgel@cbm.sc.gov.br>
2021-04-17 21:00:16 -0300

    ADD: import csv to array

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,15 +1,14 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-
-
 class ImportCategoryController {
-    constructor(private importVategoryUseCase:ImportCategoryUseCase){}
-    handle(request: Request, response: Response): Response {
-        const {file} = request;
-            this.importVategoryUseCase.execute(file)
-        return response.send();
-    }
+  constructor(private importVategoryUseCase: ImportCategoryUseCase) {}
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importVategoryUseCase.execute(file);
+    return response.send();
+  }
 }
 
-export {ImportCategoryController};
+export { ImportCategoryController };
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,11 @@
-import { ImportCategoryController } from "./ImportCategoryController";
-import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+import { ImportCategoryController } from './ImportCategoryController';
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-const importCategoryUseCase = new ImportCategoryUseCase();
-const importCategoryController = new ImportCategoryController(importCategoryUseCase);
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
 
-export {importCategoryController};
+export { importCategoryController };
