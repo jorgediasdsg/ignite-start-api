a7223f9652efd13c1c5ca0df13d1354b7ca5cb7b
Jorge Dias <jorgel@cbm.sc.gov.br>
2021-05-09 10:55:39 -0300

    ADD: jest and sample test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create Category", () => {
+  it("User should be abe sum 2+2 = 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Espero que 2+2 não seja 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
